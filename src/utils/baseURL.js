22f2cf3697f868293d0154f4bf84f483d9d16e94
mzmalik22 <zulqurnain.es7@gmail.com>
2024-02-09 19:48:13 +0500

    baseURL updated

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -5,8 +5,8 @@ if (environment === "production") {
   apiUrl = "https://gwi-backend.dev.impactscope.com";
 } else if (environment === "staging") {
   apiUrl = "https://gwi-backend-v2.impactscope.com";
-} else {
-  apiUrl = "http://localhost:5000";
+} else if (environment === "dev") {
+  apiUrl = "https://gwi-be-dev.impactscope.com";
 }
 
 export default apiUrl;
