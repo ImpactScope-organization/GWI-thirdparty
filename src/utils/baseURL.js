50b18da1ee0dde7cd3ab2382d33b48a9501c4fcc
mzmalik22 <zulqurnain.es7@gmail.com>
2024-02-16 16:02:35 +0500

    v2-avax env setyp

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,12 +1,16 @@
-const environment = "dev";
+const environment = "v2-avax";
 
 let apiUrl;
 if (environment === "production") {
   apiUrl = "https://gwi-backend.dev.impactscope.com";
 } else if (environment === "staging") {
   apiUrl = "https://gwi-backend-v2.impactscope.com";
+} else if (environment === "v2-avax") {
+  apiUrl = "https://gwi-be-avax.impactscope.com";
 } else if (environment === "dev") {
   apiUrl = "https://gwi-be-dev.impactscope.com";
+} else {
+  apiUrl = "http://localhost:5000";
 }
 
 export default apiUrl;
