8855e52699139d625723bb87d1213fd117fbb0af
yochef17 <44505384+yochef17@users.noreply.github.com>
2024-06-26 18:48:15 +0200

    Update ReviewCompleted.jsx

--- src/Components/Reports/ReviewCompleted.jsx
+++ src/Components/Reports/ReviewCompleted.jsx
@@ -428,7 +428,7 @@ const SpecificReport = () => {
               )}
               {specificReportDetailsData?.results?.IPFSHash && (
                 <a
-                  href={`https://ipfs.io/ipfs/${specificReportDetailsData?.results?.IPFSHash}`}
+                  href={`${specificReportDetailsData?.results?.IPFSHash}`}
                   target="_blank"
                   rel="noreferrer"
                   className="text-darkGreen col-span-1 truncate ml-4 text-[1em]  mb-1 font-md"
